4c68018486190bc1e29cb40c40f9b54f4614d5d5
Manish C <manish@tridemobility.com>
2023-12-16 15:47:55 +0530

    aws ip

--- src/API/api.js
+++ src/API/api.js
@@ -4,7 +4,7 @@ axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'x-csrftoken'
 const client = axios.create(
     {
-        baseURL: 'http://3.110.168.213/sourcepro/'
+        baseURL: 'http://3.110.168.213:8000/sourcepro/'
     }
 )
 export default client
